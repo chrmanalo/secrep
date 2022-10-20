1f2ed387b8917a8188a350b9fb0f733357e3985c
Christian Manalo <chr.manalo@gmail.com>
2022-10-20 18:35:51 +0800

    Add a CLI option `--render`

--- secrep/app.py
+++ secrep/app.py
@@ -2,21 +2,29 @@ import argparse
 import yaml
 from yaml.loader import SafeLoader
 
-from secrep.reporting import generate_summary_report
+from secrep.reporting import generate_summary_report, generate_detailed_report
 
 def init_args():
     parser = argparse.ArgumentParser(description='Arguments being passed to the program')
-    parser.add_argument('--summarize', '-Z', type=str, required=False, help='Generate a summary report for all vulnerabilities for each OSS')
+    parser.add_argument('--summarize', '-Z', type=str, required=False, help='Generate a summary report for all vulnerabilities for each OSS from a config file')
+    parser.add_argument('--render', '-r', type=str, required=False, help='Generate a detailed report of OSS vulnerabilities from a config file')
     return parser.parse_args()
+
+def read_file(filepath):
+    # Open the file and load the file
+    with open(filepath) as f:
+        config = yaml.load(f, Loader=SafeLoader)
+    return config
 
 def run():
     args = init_args()
 
     if args.summarize:
-        # Open the file and load the file
-        with open(args.summarize) as f:
-            config = yaml.load(f, Loader=SafeLoader)
+        config = read_file(args.summarize)
         generate_summary_report(config)
+    elif args.render:
+        config = read_file(args.render)
+        generate_detailed_report(config)
 
 if __name__ == '__main__':
     run()
